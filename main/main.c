d763d0664edeb9532f624ee714c2d46780e05048
Phil Malone <phil.malone@mr-phil.com>
2024-12-15 17:31:51 -0500

    Executable code

--- main/main.c
+++ main/main.c
@@ -8,8 +8,42 @@
 #include <nvs_flash.h>
 #include <sys/param.h>
 #include <esp_wifi.h>
+#include <driver/gpio.h>
+#include "esp_log.h"
+#include "esp_timer.h"
+#include "driver/pulse_cnt.h"
+#include <bdc_motor.h>
+#include "pid_ctrl.h"
+
 
 #define WIFI_SSID "ESP32 OTA Update"
+
+#define RED_LED   3
+#define GREEN_LED 9
+#define BLUE_LED 10
+
+#define ENC_WHEEL 13
+
+static const char *TAG = "FERT";
+
+#define BDC_MCPWM_TIMER_RESOLUTION_HZ 10000000 // 10MHz, 1 tick = 0.1us
+#define BDC_MCPWM_FREQ_HZ             25000    // 25KHz PWM
+#define BDC_MCPWM_DUTY_TICK_MAX       (BDC_MCPWM_TIMER_RESOLUTION_HZ / BDC_MCPWM_FREQ_HZ) // maximum value we can set for the duty cycle, in ticks
+#define BDC_MCPWM_GPIO_A              11
+#define BDC_MCPWM_GPIO_B              12
+
+#define BDC_ENCODER_GPIO_A            21
+#define BDC_ENCODER_GPIO_B            47
+#define BDC_ENCODER_PCNT_HIGH_LIMIT   1000
+#define BDC_ENCODER_PCNT_LOW_LIMIT    -1000
+
+#define BDC_PID_LOOP_PERIOD_MS        10   // calculate the motor speed every 10ms
+#define BDC_PID_EXPECT_SPEED          30   // expected motor speed, in the pulses counted by the rotary encoder
+
+
+//#define OUTPUT_SELECT   ( (1ULL<<RED_LED) | (1ULL<<GREEN_LED) | (1ULL<<BLUE_LED) | (1ULL<<MOTOR_A) | (1ULL<<MOTOR_B) ) 
+#define OUTPUT_SELECT   ( (1ULL<<RED_LED) | (1ULL<<GREEN_LED) | (1ULL<<BLUE_LED) ) 
+#define INPUT_SELECT ( (1ULL<<ENC_A) | (1ULL<<ENC_B) | (1ULL<<ENC_WHEEL) ) 
 
 /*
  * Serve OTA update portal (index.html)
@@ -133,6 +167,135 @@ static esp_err_t softap_init(void)
 	return res;
 }
 
+// SPEED CONTROL
+
+typedef struct {
+    bdc_motor_handle_t motor;
+    pcnt_unit_handle_t pcnt_encoder;
+    pid_ctrl_block_handle_t pid_ctrl;
+    int report_pulses;
+} motor_control_context_t;
+
+static motor_control_context_t motor_ctrl_ctx = {
+    .pcnt_encoder = NULL,
+};
+
+static void pid_loop_cb(void *args)
+{
+    static int last_pulse_count = 0;
+    motor_control_context_t *ctx = (motor_control_context_t *)args;
+    pcnt_unit_handle_t pcnt_unit = ctx->pcnt_encoder;
+    pid_ctrl_block_handle_t pid_ctrl = ctx->pid_ctrl;
+    bdc_motor_handle_t motor = ctx->motor;
+
+    // get the result from rotary encoder
+    int cur_pulse_count = 0;
+    pcnt_unit_get_count(pcnt_unit, &cur_pulse_count);
+    int real_pulses = cur_pulse_count - last_pulse_count;
+    last_pulse_count = cur_pulse_count;
+    ctx->report_pulses = real_pulses;
+
+    // calculate the speed error
+    float error = BDC_PID_EXPECT_SPEED - real_pulses;
+    float new_power = 0;
+
+
+    // set the new speed
+    pid_compute(pid_ctrl, error, &new_power);
+    bdc_motor_set_speed(motor, (uint32_t)new_power);
+
+    ESP_LOGW(TAG, "Enc %d, Speed %d, Error %f, Power %f", cur_pulse_count, real_pulses, error, new_power); 
+
+}
+
+void start_motor (void) {
+
+    ESP_LOGI(TAG, "Create DC motor");
+    bdc_motor_config_t motor_config = {
+        .pwm_freq_hz = BDC_MCPWM_FREQ_HZ,
+        .pwma_gpio_num = BDC_MCPWM_GPIO_A,
+        .pwmb_gpio_num = BDC_MCPWM_GPIO_B,
+    };
+    bdc_motor_mcpwm_config_t mcpwm_config = {
+        .group_id = 0,
+        .resolution_hz = BDC_MCPWM_TIMER_RESOLUTION_HZ,
+    };
+    bdc_motor_handle_t motor = NULL;
+    ESP_ERROR_CHECK(bdc_motor_new_mcpwm_device(&motor_config, &mcpwm_config, &motor));
+    motor_ctrl_ctx.motor = motor;
+
+    ESP_LOGI(TAG, "Init pcnt driver to decode rotary signal");
+    pcnt_unit_config_t unit_config = {
+        .high_limit = BDC_ENCODER_PCNT_HIGH_LIMIT,
+        .low_limit = BDC_ENCODER_PCNT_LOW_LIMIT,
+        .flags.accum_count = true, // enable counter accumulation
+    };
+    pcnt_unit_handle_t pcnt_unit = NULL;
+    ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
+    pcnt_glitch_filter_config_t filter_config = {
+        .max_glitch_ns = 1000,
+    };
+    ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(pcnt_unit, &filter_config));
+    pcnt_chan_config_t chan_a_config = {
+        .edge_gpio_num = BDC_ENCODER_GPIO_A,
+        .level_gpio_num = BDC_ENCODER_GPIO_B,
+    };
+    pcnt_channel_handle_t pcnt_chan_a = NULL;
+    ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_a_config, &pcnt_chan_a));
+    pcnt_chan_config_t chan_b_config = {
+        .edge_gpio_num = BDC_ENCODER_GPIO_B,
+        .level_gpio_num = BDC_ENCODER_GPIO_A,
+    };
+    pcnt_channel_handle_t pcnt_chan_b = NULL;
+    ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_b_config, &pcnt_chan_b));
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_a, PCNT_CHANNEL_EDGE_ACTION_DECREASE, PCNT_CHANNEL_EDGE_ACTION_INCREASE));
+    ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_a, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan_b, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_DECREASE));
+    ESP_ERROR_CHECK(pcnt_channel_set_level_action(pcnt_chan_b, PCNT_CHANNEL_LEVEL_ACTION_KEEP, PCNT_CHANNEL_LEVEL_ACTION_INVERSE));
+    ESP_ERROR_CHECK(pcnt_unit_add_watch_point(pcnt_unit, BDC_ENCODER_PCNT_HIGH_LIMIT));
+    ESP_ERROR_CHECK(pcnt_unit_add_watch_point(pcnt_unit, BDC_ENCODER_PCNT_LOW_LIMIT));
+    ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
+    ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
+    ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
+    motor_ctrl_ctx.pcnt_encoder = pcnt_unit;
+
+    ESP_LOGI(TAG, "Create PID control block");
+    pid_ctrl_parameter_t pid_runtime_param = {
+        .kp = 0.6,  //  0.6
+        .ki = 0.4,  //  0.4
+        .kd = 0.3,  //  0.2
+        .cal_type = PID_CAL_TYPE_INCREMENTAL,
+        .max_output   = BDC_MCPWM_DUTY_TICK_MAX - 1,
+        .min_output   = 0,
+        .max_integral = 1000,
+        .min_integral = -1000,
+    };
+    pid_ctrl_block_handle_t pid_ctrl = NULL;
+    pid_ctrl_config_t pid_config = {
+        .init_param = pid_runtime_param,
+    };
+    ESP_ERROR_CHECK(pid_new_control_block(&pid_config, &pid_ctrl));
+    motor_ctrl_ctx.pid_ctrl = pid_ctrl;
+
+    ESP_LOGI(TAG, "Create a timer to do PID calculation periodically");
+    const esp_timer_create_args_t periodic_timer_args = {
+        .callback = pid_loop_cb,
+        .arg = &motor_ctrl_ctx,
+        .name = "pid_loop"
+    };
+    esp_timer_handle_t pid_loop_timer = NULL;
+    ESP_ERROR_CHECK(esp_timer_create(&periodic_timer_args, &pid_loop_timer));
+
+    ESP_LOGI(TAG, "Enable motor");
+    ESP_ERROR_CHECK(bdc_motor_enable(motor));
+    ESP_LOGI(TAG, "Forward motor");
+    ESP_ERROR_CHECK(bdc_motor_forward(motor));
+
+    ESP_LOGI(TAG, "Start motor speed loop");
+    ESP_ERROR_CHECK(esp_timer_start_periodic(pid_loop_timer, BDC_PID_LOOP_PERIOD_MS * 1000));
+
+}
+
 void app_main(void) {
 	esp_err_t ret = nvs_flash_init();
 
@@ -156,5 +319,37 @@ void app_main(void) {
 		}
 	}
 
-	while(1) vTaskDelay(10);
+	// initialize IO
+
+	//zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+
+    //Configure outputpins
+    io_conf.mode = GPIO_MODE_OUTPUT;
+
+    //bit mask of the LED pins
+    io_conf.pin_bit_mask = OUTPUT_SELECT;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+	start_motor();
+
+	uint32_t timerMS = 0;
+	while(1) {
+		vTaskDelay(10);
+		timerMS += 100;
+
+        gpio_set_level(RED_LED,   ((timerMS / 1000) & 0x1) != 0 ? 0 : 1);
+        gpio_set_level(GREEN_LED, ((timerMS / 1000) & 0x2) != 0 ? 0 : 1);
+        gpio_set_level(BLUE_LED,  ((timerMS / 1000) & 0x4) != 0 ? 0 : 1);
+
+
+	}
 }
